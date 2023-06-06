f175487783f0a05c3aec0ffcf009c68f8aa2ddef
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-06-06 21:59:36 +0530

    "feature- Main tabular calender components"

---
+++ src/utils/helpers.js
@@ -0,0 +1,16 @@
+import dayjs from "dayjs";
+
+export function fetchMonth(month = dayjs().month()) {
+    //Math.floor rounds down the number to its next low integer or rounds to integer equal to the number;
+    month = Math.floor(month);
+    const year = dayjs().year();
+    const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
+    let currentMonthCount = 0 - firstDayOfTheMonth;
+    const daysMatrix = new Array(5).fill([]).map(() => {
+        return new Array(7).fill(null).map(() => {
+            currentMonthCount++;
+            return dayjs(new Date(year, month, currentMonthCount));
+        });
+    });
+    return daysMatrix;
+}
